33cb1b052d06cc187f6e4995759cdd46d170dd53
Eross <121542285+eross220@users.noreply.github.com>
2024-10-31 21:24:49 +0700

    speech to text demo app using web speech api and react

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Dictaphone from './Components/Dictaphone';
+
+function App() {
+  return (
+    <div className="App">
+      <Dictaphone />
+    </div>
+
+  );
+}
+
+export default App;
